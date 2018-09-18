2eed0325863f0e7dc9ca23e7449af7c4630d0277
Keith Tunstead <tunstek@tcd.ie>
2018-09-18 17:05:35 +0100

    Initial Commit

---
+++ OpenGL GLFW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL GLFW
+//
+//  Created by Keith Tunstead on 18/09/2018.
+//  Copyright © 2018 Keith Tunstead. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
